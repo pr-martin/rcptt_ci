c2707d32d8346e6b35e14fde2747bf1d7d175b1f
Paul Martin <p.r.martin.sw@gmail.com>
2015-10-19 13:18:23 -0700

    Add initial shell script and git attributes/ignores

---
+++ rcptt_clone.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Clones or Pulls from repos as appropriate.
+clear
+echo Simple Build and Test Script
+
+curdir=$PWD
+printf '%s\n' $curdir
+
+if [ -d "regressionSuite" ]; then
+   cd regressionSuite
+   git pull
+   cd ..
+else
+   git clone https://github.com/pr-martin/rcptt_ci.git regressionSuite --branch master
+fi
