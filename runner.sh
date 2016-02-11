e81ec8ac4bea53ac6e26272122352569b1c8de56
Paul Martin <pr-martin@users.noreply.github.com>
2016-02-11 10:10:49 -0800

    Update runner.sh
    Increase timeout to 4 hours

--- runner.sh
+++ runner.sh
@@ -31,4 +31,4 @@ java -jar $runnerPath/plugins/org.eclipse.equinox.launcher_1.3.100.v20150511-154
      -junitReport $junitReport \
      -import $project \
      -reuseExistingWorkspace \
-     -testOptions "testExecTimeout=3600"
+     -testOptions "testExecTimeout=14400"
